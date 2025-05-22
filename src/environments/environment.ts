55f5b37ad8a930a944dffcf8839e8000ee9d4f2e
yassinejomaa <56415445+yassinejomaa@users.noreply.github.com>
2025-05-22 21:21:18 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     apiBaseUrl:'https://pfebackend-api-fhcdfbevhehva4dx.canadacentral-01.azurewebsites.net',
-    flaskApi:'http://localhost:5000'
+    flaskApi:'https://pfe-flask-api-fpc6abdnchfnc9dm.canadacentral-01.azurewebsites.net'
 };
